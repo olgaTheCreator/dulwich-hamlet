b016d16643c26a2e9c6686797fd763b8f84322b8
ojb <curylo.olga@gmail.com>
2023-07-19 16:26:12 +0100

    working on themes

--- dulwich-hamlet-fc/src/themes/base.ts
+++ dulwich-hamlet-fc/src/themes/base.ts
@@ -2,9 +2,9 @@ import { Theme } from "../utilities/types/types";
 import { createTheme } from "./utils";
 
 export const baseTheme: Theme = createTheme({
-  primary: "blue",
-  primaryLight: "#4d4dff",
-  secondary: "red",
+  primary: "white",
+  primaryLight: "#E6FFFFFF",
+  secondary: "white",
   secondaryLight: "#ff4d4d",
-  textBase: "white",
+  textBase: "#18173C",
 });
